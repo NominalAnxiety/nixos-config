9fca1da095bd91051fba26e8d60fa90660a5e788
Austin Bowman <astrangbow2@gmai.com>
2026-04-29 11:11:26 -0700

    commit before pushing to github

--- hosts/austinPC/configuration.nix
+++ hosts/austinPC/configuration.nix
@@ -97,7 +97,7 @@
     ];
     shell = pkgs.zsh;
     openssh.authorizedKeys.keys = [
-
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOvNuLTk7y3BFUDtnOZiG4M1eia+fW9Za6recHQ8dMtU astrangbow2@gmail.com"
     ];
   };
   
@@ -106,12 +106,10 @@
   # This setups a SSH server. Very important if you're setting up a headless system.
   # Feel free to remove if you don't need it.
   services.openssh = {
-    enable = true; # enable ssh DAEMON
+    enable = true;
     settings = {
-      # Opinionated: forbid root login through SSH.
       PermitRootLogin = "no";
-      # Opinionated: use keys only.
-      # Remove if you want to SSH using passwords
+      KbdInteractiveAuthentication = false;
       PasswordAuthentication = false;
     };
   };
